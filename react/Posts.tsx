51ba90cabc7fd7bc2a799f80ab6e28f387817ffd
Matheus <matheus.goulart@penseavanti.com.br>
2022-04-19 10:28:54 -0300

    init

---
+++ react/Posts.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface Props {}
+
+const Posts: StorefrontFunctionComponent<Props> = ({}) => {
+  return (<div></div>)
+}
+
+Posts.schema = {
+  title: 'editor.countdown.title',
+  description: 'editor.countdown.description',
+  type: 'object',
+  properties: {},
+}
+
+export default Posts
